6a05fc3d70d361300feafb8663a8a3d645c0d534
Vlad Nabatov <vlad@naba.to>
2024-08-09 17:57:48 +0300

    Fixes

--- ts-to-js.mjs
+++ ts-to-js.mjs
@@ -1,40 +1,36 @@
 import dir from "node-dir"
 import fs from "fs"
-import { exec } from "child_process"
+import path from "path"
 
 const run = async () => {
-  dir.readFiles(
-    import.meta.dirname,
-    { excludeDir: ['node_modules'], match: /.tsx?$/, },
-    (err, content, filename, next) => {
-        if (err) throw err;
+    dir.readFiles(
+        import.meta.dirname,
+        { excludeDir: ["node_modules"], match: /.tsx?$/ },
+        (err, content, filename, next) => {
+            if (err) throw err
 
-        const response = fetch(
-          'http://127.0.0.1:3000/api/typescript-to-javascript',
-          { method: "POST", body: content, })
-          .then(response => response.text()
-            .then(value => fs.writeFile(filename, value, err => { if (err) console.error(err) })))
+            const response = fetch("http://127.0.0.1:3000/api/typescript-to-javascript", {
+                method: "POST",
 
-        next();
-    },
-    (err, files) => {
-        if (err) throw err;
-        console.log('finished reading files:');
-    });
+                body: content
+            }).then(response =>
+                response.text().then(value => {
+                    fs.writeFile(filename, value, err => {
+                        if (err) console.error(err)
+                    })
+                    const newFilename = path.format({ ...path.parse(filename), base: "", ext: ".js" })
+                    fs.rename(filename, newFilename, () => {
+                        console.log(`\n${filename} converted into ${newFilename}\n`)
+                    })
+                })
+            )
 
-  exec("find ./ -depth -name \"*.ts\" -o -name \"*.tsx\" -exec sh -c 'mv \"$1\" \"${1%.ts}.js\"' _ {} \\;", (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-  });
-
-
+            next()
+        },
+        (err, files) => {
+            if (err) throw err
+        }
+    )
 }
 
-run();
+run().then(r => r)
